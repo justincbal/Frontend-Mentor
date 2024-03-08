ef0c4d3f29b6e193cc40b0fe699badcd8e1c5a6d
Justin Balagtas <justinseebee@hotmail.com>
2024-03-08 01:47:16 -0500

    add empty field error check

--- age-calculator-app-main/script.js
+++ age-calculator-app-main/script.js
@@ -49,16 +49,69 @@ button.addEventListener("click", () => {
   const inYear = document.querySelector(".inYear");
   const inDays = document.querySelector(".inDay");
 
-  const date = inYear.value + "-" + inMonth.value + "-" + inDays.value;
+  if (checkErrors(inDays, inMonth, inYear)) {
+  } else {
+    console.log("reseeting");
+    // Reset styling if prior errors
+    inMonth.style.outlineColor = "hsl(0, 0%, 86%)";
+    inYear.style.outlineColor = "hsl(0, 0%, 86%)";
+    inDays.style.outlineColor = "hsl(0, 0%, 86%)";
+    const allErrorMessage = document.querySelectorAll(".errorMessage");
+    const allInput = document.querySelectorAll(".input-heading");
+    allErrorMessage.forEach((e) => {
+      e.textContent = "";
+    });
+    allInput.forEach((i) => {
+      i.classList.remove("red");
+    });
 
-  const { years, months, days } = calculateAge(date);
-  console.log(years, months, days);
+    const date = inYear.value + "-" + inMonth.value + "-" + inDays.value;
+    const { years, months, days } = calculateAge(date);
+    console.log(years, months, days);
 
-  const yearsField = document.querySelector(".years");
-  const monthsField = document.querySelector('.months');
-  const daysField = document.querySelector('.days');
+    const yearsField = document.querySelector(".years");
+    const monthsField = document.querySelector(".months");
+    const daysField = document.querySelector(".days");
 
-  yearsField.textContent = `${years} `;
-  monthsField.textContent = `${months} `;
-  daysField.textContent = `${days} `;
+    yearsField.textContent = `${years} `;
+    monthsField.textContent = `${months} `;
+    daysField.textContent = `${days} `;
+  }
 });
+
+function checkErrors(d, m, y) {
+  const allErrorMessage = document.querySelectorAll(".errorMessage");
+  const dayError = document.querySelector(".dayError");
+  const monthError = document.querySelector(".monthError");
+  const yearError = document.querySelector(".yearError");
+  let errors = false;
+  // Check if empty
+  if (d.value == "") {
+    const headDay = document.querySelector(".headDay");
+    dayError.textContent = "This field is required";
+    headDay.classList.add("red");
+    dayError.classList.add("red");
+    errors = true;
+    d.style.outlineColor = "red";
+  }
+
+  if (m.value == "") {
+    const headMonth = document.querySelector(".headMonth");
+    monthError.textContent = "This field is required";
+    headMonth.classList.add("red");
+    monthError.classList.add("red");
+    errors = true;
+    m.style.outlineColor = "red";
+  }
+
+  if (y.value == "") {
+    const headYear = document.querySelector(".headYear");
+    yearError.textContent = "This field is required";
+    headYear.classList.add("red");
+    yearError.classList.add("red");
+    errors = true;
+    y.style.outlineColor = "red";
+  }
+
+  return errors;
+}
