c8f238e8e70d04c1247c4473586572812d0bd4ba
Justin Balagtas <justinseebee@hotmail.com>
2024-03-11 02:27:41 -0400

    add navbar

---
+++ news-homepage-main/src/App.jsx
@@ -0,0 +1,13 @@
+import { useState } from "react";
+import NavBar from "./components/Navbar";
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <NavBar />
+    </>
+  );
+}
+
+export default App;
