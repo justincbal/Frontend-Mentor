7d6ccc1b7e1dc399045b3370059606020f0eea28
Justin Balagtas <justinseebee@hotmail.com>
2024-03-20 03:50:23 -0400

    add desktop design

--- tip-calculator-app-main/src/components/Output.jsx
+++ tip-calculator-app-main/src/components/Output.jsx
@@ -1,18 +1,27 @@
-import '../styles/Output.css';
+import "../styles/Output.css";
 
 const Output = () => {
-    return (
-        <>
-            <div className="outputSection">
-                <div className="output">
-                    <h2 className="outputHeader">Tip Amount</h2>
-                    <h3 className="outputSubhead">/ person</h3>
-                </div>
-                <div className="output"></div>
-                <button className="reset">RESET</button>
-            </div>
-        </>
-    )
-}
+  return (
+    <>
+      <div className="outputSection">
+        <div className="output">
+          <div className="ouputHeadings">
+            <h2 className="outputHeader">Tip Amount</h2>
+            <h3 className="outputSubhead">/ person</h3>
+          </div>
+          <h2 className="price">${}4.27</h2>
+        </div>
+        <div className="output">
+          <div className="ouputHeadings">
+            <h2 className="outputHeader">Total</h2>
+            <h3 className="outputSubhead">/ person</h3>
+          </div>
+          <h2 className="price">${}4.27</h2>
+        </div>
+        <button className="reset">RESET</button>
+      </div>
+    </>
+  );
+};
 
 export default Output;
