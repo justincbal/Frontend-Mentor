798bf0a0595872b7c92edcd54118d1e628a168f5
Justin Balagtas <justinseebee@hotmail.com>
2024-03-27 02:29:00 -0400

    add nav links

---
+++ ecommerce-product-page-main/src/components/Navbar.jsx
@@ -0,0 +1,18 @@
+import '../styles/Navbar.css'
+
+const Navbar = () => {
+    return (
+        <>
+            <div className="navBar">
+                <h1 className="navHeading">sneakers</h1>
+                <h2 className="navLinks">Collections</h2>
+                <h2 className="navLinks">Men</h2>
+                <h2 className="navLinks">Women</h2>
+                <h2 className="navLinks">About</h2>
+                <h2 className="navLinks">Contact</h2>
+            </div>
+        </>
+    )
+}
+
+export default Navbar;
